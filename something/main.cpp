c893ad132bb2d245dc1ca196355605dbc97c1222
tito <titor13@gmail.com>
2025-07-01 15:18:07 -0700

    first commit

---
+++ something/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  something
+//
+//  Created by Tito Romero on 7/1/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
